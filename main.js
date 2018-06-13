006350194d62e9573a012a716138e18cdec76446
smaragdschlange <smaragdschlange@gmx.de>
2018-06-13 21:57:04 +0200

    Changes to main.ls

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 /**
  *
- * template adapter
+ * nuki adapter
  *
  *
  *  file io-package.json comments:
  *
  *  {
  *      "common": {
- *          "name":         "template",                  // name has to be set and has to be equal to adapters folder name and main file name excluding extension
- *          "version":      "0.0.0",                    // use "Semantic Versioning"! see http://semver.org/
- *          "title":        "Node.js template Adapter",  // Adapter title shown in User Interfaces
+ *          "name":         "nuki",                     // name has to be set and has to be equal to adapters folder name and main file name excluding extension
+ *          "version":      "0.0.1",                    // use "Semantic Versioning"! see http://semver.org/
+ *          "title":        "Node.js Nuki Adapter",     // Adapter title shown in User Interfaces
  *          "authors":  [                               // Array of authord
- *              "name <mail@template.com>"
+ *              "name <smaragdschlange@gmx.de>"
  *          ]
- *          "desc":         "template adapter",          // Adapter description shown in User Interfaces. Can be a language object {de:"...",ru:"..."} or a string
+ *          "desc":         "Nuki adapter",             // Adapter description shown in User Interfaces. Can be a language object {de:"...",ru:"..."} or a string
  *          "platform":     "Javascript/Node.js",       // possible values "javascript", "javascript/Node.js" - more coming
  *          "mode":         "daemon",                   // possible values "daemon", "schedule", "subscribe"
  *          "materialize":  true,                       // support of admin3
