da20b211575826065988c81fd28c9d701cd1cd3a
smaragdschlange <smaragdschlange@gmx.de>
2020-04-13 14:00:03 +0200

    fix

--- main.js
+++ main.js
@@ -1390,14 +1390,14 @@ function get_devicetype_by_statename(_stateName) {
     adapter.log.debug('Searching for sate name ' + _stateName);
     
     for (let stateName in openerStateNames) {
-        if ( _stateName = openerStateNames[stateName] ) {
+        if ( _stateName == openerStateNames[stateName] ) {
             deviceType = 2;
         }
     }
 
     if (deviceType == 1) {
         for (stateName in lockStateNames) {
-            if ( _stateName = lockStateNames[stateName] ) {
+            if ( _stateName == lockStateNames[stateName] ) {
                 deviceType = 0;
             }
         }
